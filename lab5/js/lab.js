8a2ddaa8cf8e1cde36c4e670cf8b5e3ec2317d16
kaylchu <kylechen1114@gmail.com>
2023-10-25 15:40:04 -0700

    Lab 5

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,10 +1,23 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - JS for Lab 5
+// Author: Kyle Chen
+// Date: 10/25/2023
 
-// Constants
+// Define Variables
+var make = "Tesla";
+var model = "Model 3";
+var color = "black";
+var year = 2016;
 
-// Functions
+// calculate
+var age = 2023 - year;
+
+// output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "<br>");
+
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
